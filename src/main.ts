8b14550d3595cd5f56caaf7513761318ac4a0745
鄭 琨豑 テイコンチー cheng kunchih <100252466+Yuduki-Tei@users.noreply.github.com>
2025-07-04 08:38:01 +0000

    dark mode

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import "./style.css";
 
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import "element-plus/theme-chalk/dark/css-vars.css"; // 追加: ダークテーマ用CSS
 
 const app = createApp(App);
 app.use(createPinia());

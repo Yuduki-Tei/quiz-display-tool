a39d27041609025334aad23e72aa4bc6253f468f
Yuduki Tei <chengkunchih1998@gmail.com>
2025-07-05 13:43:41 +0900

    wrapping button and icon

---
+++ src/components/Button.vue
@@ -0,0 +1,57 @@
+<template>
+  <el-button
+    v-bind="buttonProps"
+    :class="[icon && !slots.default ? 'icon-only-btn' : '', $attrs.class]"
+    v-on="$attrs"
+  >
+    <Icon v-if="icon" :name="icon" :size="String(iconSize)" />
+    <slot v-if="!icon" />
+  </el-button>
+</template>
+
+<script setup lang="ts">
+import { defineProps, computed, useAttrs, useSlots } from "vue";
+import Icon from "@/components/Icon.vue";
+
+const props = withDefaults(
+  defineProps<{
+    icon?: string | null;
+    iconSize?: string | number;
+    type?: string;
+    size?: string;
+    circle?: boolean;
+    plain?: boolean;
+    // 其他 el-button 支援的 props 可自行擴充
+  }>(),
+  {
+    icon: null,
+    iconSize: '20',
+    type: 'default',
+    size: 'default',
+    circle: false,
+    plain: false
+  }
+);
+
+const $attrs = useAttrs();
+const slots = useSlots();
+
+const buttonProps = computed(() => ({
+  type: props.type,
+  size: props.size,
+  circle: props.circle,
+  plain: props.plain,
+  ...$attrs
+}));
+</script>
+
+<style scoped>
+.icon-only-btn {
+  padding: 0 8px;
+  min-width: 32px;
+  min-height: 32px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Icon.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-icon :size="size">
+        <component :is="iconComponent" />
+    </el-icon>
+</template>
+
+<script setup lang="ts">
+import { defineProps, computed } from "vue";
+import * as Icons from "@element-plus/icons-vue";
+
+const props = withDefaults(
+    defineProps<{
+        size?: string | null;
+        name: string;
+    }>(),
+    {
+        size: "20",
+        name: "Plus"
+    }
+);
+
+const iconComponent = computed(() => Icons[props.name]);
+</script>

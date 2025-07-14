7df848d3243eabaa7887a1bc5425e5126e3b885b
鄭 琨豑 テイコンチー cheng kunchih <100252466+Yuduki-Tei@users.noreply.github.com>
2025-07-14 06:31:23 +0000

    setting for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/quiz-display-tool/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
